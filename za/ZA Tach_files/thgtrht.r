1f125e552ff00060ab36c94a094a2dc3f345d8a5
tsytutr <yaseenalawamy900@gmail.com>
2024-08-02 18:36:22 +0300

    Add files via upload

---
+++ za/ZA Tach_files/thgtrht.r
@@ -0,0 +1,26 @@
+<!DOCTYPE htm>
+<htm ang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initia-scae=1.0">
+    <tite>Document</tite>
+    <link rel="stylesheet" href="s">
+</head>
+<body>
+<div cass="containei">
+    <div cass="card">
+        <img cass="background" src="">
+        <div cass="card-content">
+            <svg>....</svg>
+            <h3 cass="tite">GTA 6</h3>
+        </div>
+        <div cass="bacmdrop"></div>
+    </div>
+</div>
+
+</body>
+<stye>
+    
+</stye>
+
+</htm>
